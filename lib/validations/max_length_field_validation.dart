5dfccf7e3f03d16d39604031a889547a3b62f635
Bernardo Veras <bernardo@vvssistemas.com.br>
2023-03-03 16:24:54 -0300

    translate to english MaxLengthFieldValidation

--- lib/validations/max_length_field_validation.dart
+++ lib/validations/max_length_field_validation.dart
@@ -3,13 +3,13 @@ import 'package:equatable/equatable.dart';
 import '../protocols/protocols.dart';
 import '../validators/validators.dart';
 
-/// Classe responsável por validar se os campos ultrapassam um tamanho máximo.
+/// Class responsible for validating if the fields exceed a maximum size.
 class MaxLengthFieldValidation extends Equatable implements FieldLengthValidation {
-  /// Nome do campo a ser validado.
+  /// Name of the field to be validated.
   @override
   final String fieldName;
 
-  /// Tamanho máximo da String.
+  /// Maximum length.
   @override
   final int length;
 
@@ -25,14 +25,14 @@ class MaxLengthFieldValidation extends Equatable implements FieldLengthValidatio
     _maxLengthValidator = MaxLengthValidator(length: length);
   }
 
-  /// Método que valida se o valor do campo especificado é maior do que o tamanho máximo.
+  /// Method that validates whether the specified field value is greater than the maximum size.
   ///
-  /// Retorna `null` se o campo for válido ou uma `mensagem de erro` caso contrário.
+  /// Returns `null` if the field is valid, or an `error message` otherwise.
   @override
   ValidationError? validate(Map input) {
     final bool isValid = _maxLengthValidator.validate(input[fieldName]);
 
-    // Retorna a mensagem de erro se o valor for inválido, ou null se for válido.
+    // Returns an error message if the value is invalid, or null if valid.
     return isValid ? null : ValidationError.invalidField;
   }
 }
