62734aa13b3d974a595838a57cd235f1ef89ffe2
Bernardo Veras <bernardo@vvssistemas.com.br>
2023-03-03 16:27:42 -0300

    translate to english MaxLengthValidator

--- lib/validators/max_length_validator.dart
+++ lib/validators/max_length_validator.dart
@@ -1,23 +1,23 @@
 import 'validators.dart';
 
-/// Classe que verifica se a String ultrapassa um tamanho máximo.
+/// Class that checks if the String exceeds the maximum length.
 ///
-/// Como usar:
+/// Example:
 /// ```dart
 /// final validator = MaxLengthValidator(7);
 /// final text = 'example';
 /// final isValid = validator.validate(text); // true
 /// ```
 class MaxLengthValidator implements LengthValidator<String> {
-  /// Tamanho máximo da String.
+  /// Maximum length.
   @override
   final int length;
 
   MaxLengthValidator({required this.length});
 
-  /// Retorna `true` se [value] possuir um tamanho menor que [length].
+  /// Returns `true` if [value] has a length less than [length].
   ///
-  /// Caso contrário, retorna `false`.
+  /// Otherwise, it returns `false`.
   @override
   bool validate(String? value) {
     if (value == null || value.isEmpty) {
