ba71fc2d29ecb50682f054f3009ba93e9be753ba
Bernardo Veras <bernardo@vvssistemas.com.br>
2023-03-03 14:05:25 -0300

    add ValidationComposite docs

--- lib/validation/validators/validation_composite.dart
+++ lib/validation/validators/validation_composite.dart
@@ -1,14 +1,20 @@
 import '../protocols/protocols.dart';
 
+/// Classe responsável por compor uma lista de validações de campo e verificar se todas as validações são válidas.
 class ValidationComposite implements Validation {
-  ValidationComposite(this.validations);
-
+  /// Lista de validações de campo a serem verificadas.
   final List<FieldValidation> validations;
 
+  ValidationComposite(this.validations);
+
+  /// Método que verifica se todas as validações de campo são válidas para o campo especificado.
+  ///
+  /// Retorna `null` se todas as validações forem válidas ou uma `mensagem de erro` caso contrário.
   @override
   String? validate({required String field, required String? value}) {
     String? error;
 
+    // Itera sobre as validações de campo que se aplicam ao campo especificado
     for (final validation in validations.where((v) => v.field == field)) {
       error = validation.validate(value);
 
@@ -16,6 +22,8 @@ class ValidationComposite implements Validation {
         return error;
       }
     }
+
+    // Retorna null se todas as validações foram bem sucedidas
     return null;
   }
 }
